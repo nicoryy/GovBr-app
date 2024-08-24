67c267c4fb05677f935c174bdcb8d56a863dc68e
nicoryy <pedronicory@gmail.com>
2024-08-24 11:51:35 -0300

    added blurView

--- app/_layout.js
+++ app/_layout.js
@@ -4,14 +4,20 @@ import { SafeAreaView } from "react-native-safe-area-context";
 
 import "../global.css";
 
+import { BlurView } from "expo-blur";
+
 export default function Layout() {
   return (
     <SafeAreaView>
       <ImageBackground source={require("../assets/Frame 13.png")}>
         <View className="h-screen justify-end">
-          <View className="h-[475px] backdrop-filter backdrop-blur-sm bg-pp_bsbg rounded-t-[30px] flex justify-center items-center ">
+          <BlurView
+            intensity={90}
+            experimentalBlurMethod="dimezisBlurView"
+            className="h-[475px] bg-pp_bsbg rounded-t-[30px] overflow-hidden flex justify-center items-center "
+          >
             <Slot />
-          </View>
+          </BlurView>
         </View>
       </ImageBackground>
     </SafeAreaView>
