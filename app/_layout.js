8416deaa365a3d5df03df6f6897c8970c960cf97
nicory <pedronicory@gmail.com>
2024-08-26 21:18:48 -0300

    customazing font

--- app/_layout.js
+++ app/_layout.js
@@ -1,12 +1,38 @@
 import { View, ImageBackground } from "react-native";
-import { Slot } from "expo-router";
+import { Slot, SplashScreen } from "expo-router";
 import { SafeAreaView } from "react-native-safe-area-context";
+import * as Font from 'expo-font';
 
 import "../global.css";
 
 import { BlurView } from "expo-blur";
+import { useEffect, useState } from "react";
 
 export default function Layout() {
+  const [fontsLoaded, setFontsLoaded] = useState(false);
+
+  useEffect(() => {
+    async function loadFonts() {
+      try {
+        await SplashScreen.preventAutoHideAsync(); // Prevent the splash screen from hiding
+        await Font.loadAsync({
+          'SF Pro': require('../assets/fonts/SF Pro.ttf'),
+          // You can load multiple fonts here
+        });
+        setFontsLoaded(true);
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        await SplashScreen.hideAsync(); // Hide the splash screen after fonts are loaded
+      }
+    }
+
+    loadFonts();
+  }, []);
+
+  if (!fontsLoaded) {
+    return null; // Return null or a loading component until fonts are loaded
+  }
   return (
     <SafeAreaView edges={["top"]}>
       <ImageBackground source={require("../assets/Frame 13.png")}>
--- app/index.js
+++ app/index.js
@@ -3,7 +3,7 @@ import { Text, View, TouchableHighlight } from "react-native";
 export default function Page() {
   return (
     <View className="flex h-full w-full items-center justify-start gap-12">
-      <Text className="text-5xl leading-tight w-[323px] py-16 text-center font-medium">
+      <Text className="text-5xl leading-tight w-[323px] py-16 text-center font-sf font-semibold">
         A confiança de um governo seguro
       </Text>
 
